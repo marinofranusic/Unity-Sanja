d61216324f5a625fc862f5f6d83d72a4cf6f8482
marinofranusic <marinofranusic@gmail.com>
2015-09-23 09:03:34 +0200

    Level forwarding
    Script used for advancing or repeating the level

---
+++ DaljeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DaljeScript : MonoBehaviour {
+
+	public void Dalje()
+	{
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+	
+	public void PonoviNivo()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
